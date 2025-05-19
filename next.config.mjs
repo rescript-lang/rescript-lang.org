5ba33f66c71185be42eef30a88ff813cb9a638f6
Hyeseong Kim <hey@hyeseong.kim>
2025-05-20 01:26:26 +0900

    ensure out dir before touch

--- next.config.mjs
+++ next.config.mjs
@@ -156,7 +156,7 @@ const config = {
 
     if (process.env.NODE_ENV === "production") {
       const redirectsFile = path.join(import.meta.dirname, "out/_redirects");
-      await fs.mkdir(path.dirname(redirectsFile));
+      await fs.mkdir(path.dirname(redirectsFile), { recursive: true });
       await fs.writeFile(
         redirectsFile,
         redirects
