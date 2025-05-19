2f524898bc2363559322ad84bb9f94d5a6a8a94f
Hyeseong Kim <hey@hyeseong.kim>
2025-05-20 00:13:22 +0900

    Sync Next.js redirects to Cloudflare

---
+++ scripts/sync-redirects.mjs
@@ -0,0 +1,25 @@
+import * as path from "node:path";
+import * as fs from "node:fs";
+
+import nextConfig from "../next.config.mjs";
+
+const redirectsConfig = await nextConfig.redirects();
+
+/**
+ * @param {{
+ *   source: string,
+ *   destination: string,
+ *   permanent: boolean,
+ * }} config 
+ * @return {string}
+ */
+function lineFormat({
+  source,
+  destination,
+  permanent,
+}) {
+  return `${source}  ${destination}  ${permanent ? 308 : 307}`;
+}
+
+const redirects = redirectsConfig.map(lineFormat).join("\n");
+const redirectsFile = path.join(import.meta.dirname, "../public/_redirects");
