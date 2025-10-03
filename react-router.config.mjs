5282ad2f7ed955d5a03dc3789fffd79eb15662b5
Josh Derocher-Vlk <josh@vlkpack.com>
2025-10-03 16:37:04 -0400

    copy to out folder

--- react-router.config.mjs
+++ react-router.config.mjs
@@ -1,9 +1,15 @@
+import * as fs from "node:fs";
 import { init } from "react-router-mdx/server";
+
 const mdx = init({ paths: ["_blogposts", "docs"], aliases: ["blog", "docs"] });
 
 export default {
   ssr: false,
+
   async prerender({ getStaticPaths }) {
     return [...(await getStaticPaths()), ...(await mdx.paths())];
+  },
+  buildEnd: async () => {
+    fs.cpSync("./build/client", "./out", { recursive: true });
   },
 };
