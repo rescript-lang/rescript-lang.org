7dc4b86e69cbb3144bea08175478e213ba7b9197
Josh Vlk <josh@vlkpack.com>
2025-10-24 15:57:04 -0400

    fix doc tests

--- scripts/test-examples.mjs
+++ scripts/test-examples.mjs
@@ -1,68 +1,16 @@
-import glob from "glob";
 import fs from "fs";
-import child_process from "child_process";
+import glob from "glob";
 import path from "path";
 import { URL } from "url";
+import child_process from "child_process";
 
 const pathname = new URL(".", import.meta.url).pathname;
 const __dirname =
   process.platform !== "win32" ? pathname : pathname.substring(1);
 
-let tempFileName = path.join(__dirname, "..", "_tempFile.res");
+let tempFileName = path.join(__dirname, "../temp/src/", "_tempFile.res");
 let tempFileNameRegex = /_tempFile\.res/g;
 
-// TODO: In the future we need to use the appropriate rescript version for each doc version variant
-//       see the package.json on how to define another rescript version
-let compilersDir = path.join(__dirname, "..", "compilers");
-
-let bsc = path.join(
-  compilersDir,
-  "node_modules",
-  "rescript-1110",
-  process.platform,
-  "bsc.exe",
-);
-let rescriptBin = path.join(
-  compilersDir,
-  "node_modules",
-  "rescript-1110",
-  "rescript",
-);
-let rescriptCoreCompiled = path.join(
-  compilersDir,
-  "node_modules",
-  "@rescript",
-  "core",
-  "lib",
-  "ocaml",
-);
-let rescriptReactCompiled = path.join(
-  compilersDir,
-  "node_modules",
-  "@rescript",
-  "react",
-  "lib",
-  "ocaml",
-);
-
-const prepareCompilers = () => {
-  if (fs.existsSync(bsc)) {
-    return;
-  }
-  console.log("compilers not installed. Installing compilers...");
-  child_process.execFileSync("npm", ["install"], { cwd: compilersDir });
-};
-
-const prepareDependencies = () => {
-  if (
-    fs.existsSync(rescriptCoreCompiled) &&
-    fs.existsSync(rescriptReactCompiled)
-  ) {
-    return;
-  }
-  console.log("Dependencies not installed. Installing...");
-  child_process.execFileSync(rescriptBin, [], { cwd: compilersDir });
-};
 
 let parseFile = (content) => {
   if (!/```res (example|prelude|sig)/.test(content)) {
@@ -71,6 +19,7 @@ let parseFile = (content) => {
 
   let inCodeBlock = false;
   let moduleId = 0;
+
   return content
     .split("\n")
     .map((line) => {
@@ -98,33 +47,55 @@ let parseFile = (content) => {
     .join("\n");
 };
 
-let postprocessOutput = (file, error) => {
-  return error.stderr
-    .toString()
-    .replace(tempFileNameRegex, path.relative(".", file))
-    .replace(
-      /\/\* _MODULE_(EXAMPLE|PRELUDE|SIG)_START \*\/.+/g,
-      (_, capture) => {
-        return (
-          "```res " +
-          (capture === "EXAMPLE"
-            ? "example"
-            : capture === "PRELUDE"
-              ? "prelude"
-              : "sig")
-        );
-      },
-    )
-    .replace(/(.*)\}(.*)\/\/ _MODULE_END/g, (_, cap1, cap2) => {
-      // cap1 cap2 might be empty or ansi coloring code
-      return cap1 + "```" + cap2;
-    });
-};
 
-prepareCompilers();
-prepareDependencies();
+// TODO post RR7: revisit this
+// let postprocessOutput = (file, error) => {
+//   return error.stderr
+//     .toString()
+//     .replace(tempFileNameRegex, path.relative(".", file))
+//     .replace(
+//       /\/\* _MODULE_(EXAMPLE|PRELUDE|SIG)_START \*\/.+/g,
+//       (_, capture) => {
+//         return (
+//           "```res " +
+//           (capture === "EXAMPLE"
+//             ? "example"
+//             : capture === "PRELUDE"
+//               ? "prelude"
+//               : "sig")
+//         );
+//       },
+//     )
+//     .replace(/(.*)\}(.*)\/\/ _MODULE_END/g, (_, cap1, cap2) => {
+//       // cap1 cap2 might be empty or ansi coloring code
+//       return cap1 + "```" + cap2;
+//     });
+// };
 
 console.log("Running tests...");
+
+
+let rescriptJson = `{
+    "name": "temp",
+    "namespace": false,
+    "jsx": {
+      "version": 4
+    },
+    "dependencies": [
+        "@rescript/react"
+    ],
+    "warnings": {
+        "number": "-109-27-32"
+    },
+    "sources": [
+        {
+            "dir": "src"
+        }
+    ]
+}`
+
+fs.mkdirSync(path.join(__dirname, "../temp/src/"), { recursive: true });
+fs.writeFileSync(path.join(__dirname, "../temp/rescript.json"), rescriptJson);
 fs.writeFileSync(tempFileName, "");
 
 let success = true;
@@ -135,28 +106,15 @@ glob.sync(__dirname + "/../docs/{manual,react}/**/*.mdx").forEach((file) => {
   if (parsedResult != null) {
     fs.writeFileSync(tempFileName, parsedResult);
     try {
+      console.log("testing examples in", file)
       // -109 for suppressing `Toplevel expression is expected to have unit type.`
       // Most doc snippets do e.g. `Belt.Array.length(["test"])`, which triggers this
-      child_process.execFileSync(
-        bsc,
-        [
-          tempFileName,
-          "-I",
-          rescriptCoreCompiled,
-          "-I",
-          rescriptReactCompiled,
-          "-bs-jsx",
-          "4",
-          "-w",
-          "-109",
-          "-uncurried",
-          "-open",
-          "RescriptCore",
-        ],
-        { stdio: "pipe" },
+      child_process.execSync(
+        "npm exec rescript build ./temp -- --quiet",
+        { stdio: "inherit" },
       );
     } catch (e) {
-      process.stdout.write(postprocessOutput(file, e));
+      // process.stdout.write(postprocessOutput(file, e));
       success = false;
     }
   }
@@ -164,3 +122,5 @@ glob.sync(__dirname + "/../docs/{manual,react}/**/*.mdx").forEach((file) => {
 
 fs.unlinkSync(tempFileName);
 process.exit(success ? 0 : 1);
+
+
