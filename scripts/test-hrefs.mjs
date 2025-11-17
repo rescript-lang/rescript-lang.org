42b95e929ac4cae35631a71b5c3f0beae87f2180
Josh Vlk <josh@vlkpack.com>
2025-11-17 17:09:55 -0500

    fix mdx links

--- scripts/test-hrefs.mjs
+++ scripts/test-hrefs.mjs
@@ -9,15 +9,12 @@ const files = new Set(...[(await fs.readdir('markdown-pages', { recursive: true 
 for (const file of files) {
 
   if (file.includes(".mdx")) {
-    // console.log(file.name)
-
-    // let filePath = file.parentPath + '/' + file.name
 
     let result = await remark()
       .use(remarkValidateLinks)
       .process(await read('markdown-pages/' + file))
 
-    console.log(reporter(result))
+    console.log(reporter(result, { quiet: true }))
   }
 }
 
