236235aaa465a26e118e7e21c230b8055239e711
Josh Derocher-Vlk <josh@vlkpack.com>
2025-11-25 19:32:29 -0500

    format

--- image-converter.config.mjs
+++ image-converter.config.mjs
@@ -1,9 +1,9 @@
 export default {
-    dir: "public",            // Directory to scan for images
-    converted: "*.{png,jpg,jpeg}", // Glob pattern for source image files to convert
-    format: "avif",           // Output image format: 'webp' or 'avif'
-    quality: 80,              // Quality of output images (0–100)
-    recursive: true,          // Whether to search subdirectories recursively
-    removeOriginal: true,     // Delete original files after successful conversion
-    ignoreOnStart: false,     // If true, ignore existing files on watcher startup
+  dir: "public", // Directory to scan for images
+  converted: "*.{png,jpg,jpeg}", // Glob pattern for source image files to convert
+  format: "avif", // Output image format: 'webp' or 'avif'
+  quality: 80, // Quality of output images (0–100)
+  recursive: true, // Whether to search subdirectories recursively
+  removeOriginal: true, // Delete original files after successful conversion
+  ignoreOnStart: false, // If true, ignore existing files on watcher startup
 };
